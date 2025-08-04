a3e2cf5e4efdd072cd314037ca73974a003a95c2
LeoPedroso2323 <leonardopedroso857@gmail.com>
2025-08-04 16:08:24 -0300

    04-08

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -5,6 +5,7 @@ const orgController = require("../controllers/orgController");
 const eventoController = require("../controllers/eventoController");
 const ingController = require("../controllers/ingController");
 const compraController = require("../controllers/compraController")
+const upload = require("../services/upload")
 
 router.post("/user", userController.createUser);
 router.get("/user", verifyJWT, userController.getAllUsers);
@@ -17,7 +18,7 @@ router.get("/org", orgController.getAllOrgs);
 router.put("/org", orgController.updateOrg);
 router.delete("/org/:id_organizador", orgController.deleteOrg);
 
-router.post("/evento", eventoController.createEvento);
+router.post("/evento", upload.single("imagem"), eventoController.createEvento);
 router.get("/evento", eventoController.getAllEventos);
 router.get("/evento/data", eventoController.getEventosPorData); 
 router.get("/evento/:data", eventoController.getEventosPorData7Dias);
