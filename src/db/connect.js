e5c90db68e95e4b9d734949df5dc26aed476047a
LeoPedroso2323 <leonardopedroso857@gmail.com>
2025-09-29 14:06:30 -0300

    .

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQLHOST || DB_HOST,
-  user: process.env.MYSQLHOST || DB_USER,
+  user: process.env.MYSQLUSER || DB_USER,
   password: process.env.MYSQLPASSWORD || DB_PASSWORD,
-  database: process.env.MYSQLHOST || CDB_NAME,
+  database: process.env.MYSQLDATABASE || CDB_NAME,
   port: process.env.MYSQLPORT || 3306,
 });
 
