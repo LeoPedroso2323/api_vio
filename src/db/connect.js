d0fc45dc88bf02f5957872abcc230fe2a83cf558
LeoPedroso2323 <leonardopedroso857@gmail.com>
2025-09-29 13:16:23 -0300

    alteração connect.js

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  connectionLimit: 10,
+  host: process.env.MYSQLHOST || DB_HOST,
+  user: process.env.MYSQLHOST || DB_USER,
+  password: process.env.MYSQLPASSWORD || DB_PASSWORD,
+  database: process.env.MYSQLHOST || CDB_NAME,
+  port: process.env.MYSQLPORT || 3306,
 });
 
 module.exports = pool;
